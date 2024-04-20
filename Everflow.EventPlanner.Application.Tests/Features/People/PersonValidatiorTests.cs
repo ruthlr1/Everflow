fc6f2498c82010bf28d9ad6d907aba8262073cc7
Ruds <lachlanrutherford@gmail.com>
2024-04-21 10:25:10 +1200

    Added unit tests

---
+++ Everflow.EventPlanner.Application.Tests/Features/People/PersonValidatiorTests.cs
@@ -0,0 +1,78 @@
+﻿using Everflow.EventPlanner.Application.Features.People.Upsert;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everflow.EventPlanner.Application.Tests.Features.People
+{
+    public class PersonValidatiorTests
+    {
+        private UpsertPersonCommandValidator _validator;
+        private UpsertPersonCommand _model;
+        [SetUp]
+        public void SetupValidator()
+        {
+            _model = new UpsertPersonCommand();
+            _validator = new UpsertPersonCommandValidator();
+        }
+
+
+        [Test]
+        public void EmptyModel_ShouldHaveValidation()
+        {
+            // act
+            TestValidationResult<UpsertPersonCommand> result = _validator.TestValidate(_model);
+
+            // assert
+            result.ShouldHaveValidationErrorFor(x => x.FirstName);
+            result.ShouldHaveValidationErrorFor(x => x.LastName);
+            result.ShouldHaveValidationErrorFor(x => x.EmailAddress);
+        }
+
+        [Test]
+        public void ModelWithInvalidEmail_ShouldHaveValidation()
+        {
+            // arrange
+            _model.EmailAddress = "sdahikkans";
+
+            // act
+            TestValidationResult<UpsertPersonCommand> result = _validator.TestValidate(_model);
+
+            // assert
+            result.ShouldHaveValidationErrorFor(x => x.EmailAddress);
+        }
+
+        [Test]
+        public void ModelWithValidEmail_ShouldNotHaveValidation()
+        {
+            // arrange
+            _model.EmailAddress = "johndoe@gmail.com";
+
+            // act
+            TestValidationResult<UpsertPersonCommand> result = _validator.TestValidate(_model);
+
+            // assert
+            result.ShouldNotHaveValidationErrorFor(x => x.EmailAddress);
+        }
+
+        [Test]
+        public void ValidModel_ShouldNotHaveValidation()
+        {
+            // arrange
+            _model.FirstName = "John";
+            _model.LastName = "Doe";
+            _model.EmailAddress = "johndoe@gmail.com";
+
+            // act
+            TestValidationResult<UpsertPersonCommand> result = _validator.TestValidate(_model);
+
+            // assert
+            result.ShouldNotHaveValidationErrorFor(x => x.FirstName);
+            result.ShouldNotHaveValidationErrorFor(x => x.LastName);
+            result.ShouldNotHaveValidationErrorFor(x => x.EmailAddress);
+        }
+    }
+}
--- Everflow.EventPlanner.Application/Features/People/Upsert/UpsertPersonCommandValidator.cs
+++ Everflow.EventPlanner.Application/Features/People/Upsert/UpsertPersonCommandValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,15 @@ namespace Everflow.EventPlanner.Application.Features.People.Upsert
             RuleFor(x => x.EmailAddress).NotEmpty();
             RuleFor(x => x.Password).NotEmpty();
 
+            When(x => !string.IsNullOrEmpty(x.EmailAddress), () => {
+                RuleFor(x => x.EmailAddress).NotEmpty().Must(x => IsValidEmail(x)).WithMessage("Email address is invalid");
+            });
+        }
+
+        private bool IsValidEmail(string? email)
+        {
+            var result =  new EmailAddressAttribute().IsValid(email);
+            return result;
         }
     }
 }
